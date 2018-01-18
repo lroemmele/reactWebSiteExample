175f8f40db70d22b85c818db74cf2e5111fa91c6
lroemmele <lroemmele@gmail.com>
2018-01-18 10:44:16 -0500

    add bootstrap nav to header

--- examplesite/src/Components/headerComponent/header.js
+++ examplesite/src/Components/headerComponent/header.js
@@ -6,25 +6,45 @@ class Header extends Component {
   render() {
     return (
       <header>
-        <div className="banner">
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
+       <div className="container-fluid">
+
+        {/*collapse navbar at 576px for mobile view*/}
+        <nav className="navbar navbar-expand-sm navbar-light ">
+
+          {/*brand logo image*/}
+          <Link className="navbar-brand" to="/"><img src="https://cdn.pixabay.com/photo/2017/09/23/21/24/label-2780155__480.png" width="100px"/></Link>
+
+          {/*toggle mobile button on below 576px*/}
+          <button className="navbar-toggler" type="button"
+            data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+
+          <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul className="navbar-nav ">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/">Home</Link>
               </li>
-              <li>
-                <Link to="/Products">Products</Link>
+              <li className="nav-item">
+                <Link className="nav-link" to="/Products">Products</Link>
               </li>
-              <li>
-                <Link to="/Aboutpage">About Us</Link>
+              <li className="nav-item">
+                <Link className="nav-link" to="/Aboutpage">About Us</Link>
               </li>
-              <li>
-                <Link to="/Contact">Contact Us</Link>
+              <li className="nav-item">
+                <Link className="nav-link" to="/Contact">Contact Us</Link>
               </li>
             </ul>
-          </nav>
-        </div>
 
+
+          </div>
+          </nav>
+
+        </div>
       </header>
     );
   }
