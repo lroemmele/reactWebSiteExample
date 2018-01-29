8f16a53e85b8bd96db25753f8e1c2384a88515e2
lroemmele <lroemmele@gmail.com>
2018-01-29 08:41:38 -0500

    update to header and homepage

--- examplesite/src/Components/headerComponent/header.js
+++ examplesite/src/Components/headerComponent/header.js
@@ -14,42 +14,44 @@ class Header extends Component {
     return (
 
        <div className="container-fluid">
-        <div className="jumbotron">
+        <div className="bg">
+          <div className="jumbotron">
 
-        {/*collapse navbar at 576px for mobile view*/}
-        <nav className="navbar navbar-expand-sm navbar-light">
+          {/*collapse navbar at 576px for mobile view*/}
+          <nav className="navbar navbar-expand-sm navbar-light">
 
-          {/*brand logo image*/}
-          <Link className="navbar-brand" to="/"><img src="https://logopond.com/logos/4a614224e9d4a4fb2c629fd3d18208a9.png" width="150px"/></Link>
+            {/*brand logo image*/}
+            <Link className="navbar-brand" to="/"><img src="https://logopond.com/logos/4a614224e9d4a4fb2c629fd3d18208a9.png" width="150px"  /></Link>
 
-          {/*toggle mobile button on below 576px*/}
-          <button className="navbar-toggler" type="button"
-            data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false"
-            aria-label="Toggle navigation">
+            {/*toggle mobile button on below 576px*/}
+            <button className="navbar-toggler" type="button"
+              data-toggle="collapse" data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent" aria-expanded="false"
+              aria-label="Toggle navigation">
 
-            <span className="navbar-toggler-icon"></span>
-          </button>
+              <span className="navbar-toggler-icon"></span>
+            </button>
 
-            {/*nav links*/}
-            <div className="collapse navbar-collapse " id="navbarSupportedContent">
-              <ul className="navbar-nav ">
-                <li className="nav-item active">
-                  <Link className="nav-link" to="/" onClick={this.collapseNavMenu} >Home</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/Products" onClick={this.collapseNavMenu}>Products</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/Aboutpage" onClick={this.collapseNavMenu} >About Us</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/Contact" onClick={this.collapseNavMenu} >Contact Us</Link>
-                </li>
-              </ul>
-            </div>
+              {/*nav links*/}
+              <div className="collapse navbar-collapse " id="navbarSupportedContent">
+                <ul className="navbar-nav ">
+                  <li className="nav-item active">
+                    <Link className="nav-link" to="/" onClick={this.collapseNavMenu} >Home</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/Products" onClick={this.collapseNavMenu}>Products</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/Aboutpage" onClick={this.collapseNavMenu} >About Us</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/Contact" onClick={this.collapseNavMenu} >Contact Us</Link>
+                  </li>
+                </ul>
+              </div>
 
-          </nav>
+            </nav>
+          </div>
         </div>
       </div>
     );
